5afb472a27f2715bb01ed8655ffb53b461ee20bb
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2019-09-05 10:15:13 +0200

    Update asana_points.user.js

--- asana_points.user.js
+++ asana_points.user.js
@@ -10,68 +10,67 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
+    'use strict';
 
-	let tallyTotal;
+    function _getHeaders(column) {
+        const header = column.querySelector('.BoardColumnHeader .BoardColumnHeaderTitle');
+        if (!header.querySelector('.js-total-points')) {
+            header.innerHTML += '<span class="js-total-points Pill--colorYellowGreen Pill--deuteranopia Pill Pill--small"></span>'
+        }
+        if (!header.querySelector('.js-estimate-points')) {
+            header.innerHTML += '<span class="js-estimate-points Pill--colorBlue Pill--deuteranopia Pill Pill--small"></span>'
+        }
 
-	function _getHeaders(column) {
-		const header = column.querySelector('.BoardColumnHeader .BoardColumnHeaderTitle');
-		if (!header.querySelector('.js-total-points')) {
-			header.innerHTML += '<span class="js-total-points Pill--colorYellowGreen Pill--deuteranopia Pill Pill--small"></span>'
-		}
-		if (!header.querySelector('.js-estimate-points')) {
-			header.innerHTML += '<span class="js-estimate-points Pill--colorBlue Pill--deuteranopia Pill Pill--small"></span>'
-		}
+        let total = header.querySelector('.js-total-points');
+        let estimateTotal = header.querySelector('.js-estimate-points');
+        return {
+            total,
+            estimateTotal
+        };
+    }
 
-		let total = header.querySelector('.js-total-points');
-		let estimateTotal = header.querySelector('.js-estimate-points');
-		return {
-			total,
-			estimateTotal
-		};
-	}
+    function _countPoints(column) {
+        let total = 0;
+        let estimateTotal = 0;
+        [...column.querySelectorAll('.CustomPropertyEnumPreviewCell.BoardCardCustomPropertyPreviewCell-cell')].forEach(i => {
+            let titleValue = i.getAttribute('title');
+            //Check if it is an estimate
+            const [_, value, isEstimate] = titleValue.match(/^([0-9.]+)(E$)?/) || [];
+            if (!isNaN(value)) {
+                if (isEstimate) {
+                    estimateTotal += +value;
+                } else {
+                    total += +value;
+                }
+            }
+        });
+        return {total, estimateTotal};
+    }
 
-	function _countPoints(column) {
-		let total = 0;
-		let estimateTotal = 0;
-		[...column.querySelectorAll('.CustomPropertyEnumPreviewCell.BoardCardCustomPropertyPreviewCell-cell')].forEach(i => {
-			let titleValue = i.getAttribute('title');
-			//Check if it is an estimate
-			const [_, value, isEstimate] = titleValue.match(/^([0-9.]+)(E$)?/) || [];
-			if (!isNaN(value)) {
-				if (isEstimate) {
-					estimateTotal += +value;
-				} else {
-					total += +value;
-				}
-			}
-		});
-		return {total, estimateTotal};
-	}
+    function tally() {
+        let tallyTotal = document.getElementById('tallys');
+        if (!tallyTotal) {
+            tallyTotal = document.createElement('span');
+            tallyTotal.id = "tallys";
+            document.querySelector('.PageToolbarStructure-leftChildren').appendChild(tallyTotal);
+        }
+        let total = 0;
+        let estimatedTotal = 0;
+        let columns = [...document.querySelectorAll('.BoardColumn')];
+        columns.forEach(col => {
+            const headers = _getHeaders(col);
+            const points = _countPoints(col);
 
-	function tally() {
-		if (tallyTotal === false) {
-			tallyTotal = document.createElement('span');
-			document.querySelector('.PageToolbarStructure-leftChildren').appendChild(tallyTotal);
+            headers.total.innerText = points.total;
+            headers.estimateTotal.innerText = points.estimateTotal
 
-		}
-		let total = 0;
-		let estimatedTotal = 0;
-		let columns = [...document.querySelectorAll('.BoardColumn')];
-		columns.forEach(col => {
-			const headers = _getHeaders(col);
-			const points = _countPoints(col);
+            total += points.total;
+            estimatedTotal+=points.estimateTotal;
 
-			headers.total.innerText = points.total;
-			headers.estimateTotal.innerText = points.estimateTotal
+        });
 
-			total += points.total;
-			estimatedTotal+=points.estimateTotal;
+        tallyTotal.innerHTML = `Estimated: ${estimatedTotal}      Actual: ${total}`;
+    }
 
-			tallyTotal.innerHTML = `Estimated: ${estimatedTotal}      Total: ${total}`;
-		});
-	}
-
-	tally();
-	setInterval(tally, 1000);
+    setInterval(tally, 5000);
 })();

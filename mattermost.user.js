727bfd2f9a4069850c34fa5d6218a093c0f3fded
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2021-03-30 11:32:13 +0200

    Update mattermost.user.js

--- mattermost.user.js
+++ mattermost.user.js
@@ -2,12 +2,13 @@
 // @name         Mattermost favicon and hover
 // @namespace    https://krakaw.github.io/
 // @updateURL    https://github.com/Krakaw/tampermonkey/raw/master/mattermost.user.js
-// @version      0.7
+// @version      0.9
 // @description  Favicon change when there are unread messages, when hovering over a name, expose all members of private groups
 // @author       Krakaw
 // @match        https://nomatter.tari.com/*
 // @grant        GM_notification
 // @grant        window.focus
+// @grant unsafeWindow
 // ==/UserScript==
 
 
@@ -21,6 +22,7 @@ GM_notification(notificationDetails);*/
 
 (function (document) {
     'use strict';
+    console.log('Tampermonkey script init');
     var logoColor = "0089de";
     var sideBarColors = {
         new: "rgba(255,25,25,0.7)",
@@ -77,76 +79,82 @@ GM_notification(notificationDetails);*/
         });
     }
 
-    window.addEventListener('load', function () {
-        document.body.addEventListener("click", function (event) {
-            if (event.target.classList.contains("unread") && event.target.classList.contains("team-container")) {
-                event.target.classList.remove("unread");
-            }
-        });
 
-        setTimeout(function () {
-            console.log("Running Tampermonkey mods");
-            var sideBarEl = document.querySelector('.team-sidebar');
+    document.body.addEventListener("click", function (event) {
+        if (event.target.classList.contains("unread") && event.target.classList.contains("team-container")) {
+            event.target.classList.remove("unread");
+        }
+    });
+
+    function initialize() {
+        var sideBarEl = document.querySelector('.team-sidebar');
+        if (!sideBarEl) {
+            setTimeout(initialize, 10000);
+            return;
+        }
+        console.log("Running Tampermonkey mods");
 
 
 
-            var canvas = document.createElement('canvas'),
-                ctx,
-                img = document.createElement('img'),
-                links = document.querySelectorAll("head link[rel=icon]"),
-                channelGroups = document.querySelectorAll('#sidebarChannelContainer ul li.sidebar-section__header');
 
-            channelGroups.forEach(group => {
-                group.addEventListener('click', (e) => {
-                   group.parentElement.classList.toggle("override-visible");
-                });
+        var canvas = document.createElement('canvas'),
+            ctx,
+            img = document.createElement('img'),
+            links = document.querySelectorAll("head link[rel=icon]"),
+            channelGroups = document.querySelectorAll('#sidebarChannelContainer ul li.sidebar-section__header');
 
+        channelGroups.forEach(group => {
+            group.addEventListener('click', (e) => {
+                group.parentElement.classList.toggle("override-visible");
             });
-            var origLink = links[0].href;
 
-            var setTitles = function () {
-                document.querySelectorAll('.sidebar-item__name').forEach(function (item) {
-                    if (item) {
-                        item.parentElement.parentElement.setAttribute("title", item.innerText.trim())
+        });
+        var origLink = links[0].href;
+
+        var setTitles = function () {
+            document.querySelectorAll('.sidebar-item__name').forEach(function (item) {
+                if (item) {
+                    item.parentElement.parentElement.setAttribute("title", item.innerText.trim())
+                }
+            });
+        };
+        setTitles();
+        setInterval(setTitles, 60000);
+
+
+        setInterval(function () {
+            const unreadElements = document.querySelectorAll('.SidebarChannel .unread-title');
+            createDirectUnreadLinks(sideBarEl, unreadElements);
+
+            var unreadCount = unreadElements.length
+            + document.querySelectorAll('.team-btn .badge').length
+            + document.querySelectorAll('.team-container.unread').length;
+            canvas.height = canvas.width = 16; // set the size
+            ctx = canvas.getContext('2d');
+            img.onload = function () { // once the image has loaded
+                ctx.drawImage(this, 0, 0);
+                sideBarEl.style.backgroundColor = sideBarColors.old;
+                if (unreadCount > 0) {
+                    sideBarEl.style.backgroundColor = sideBarColors.new;
+                    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                    var data = imageData.data;
+                    for (var i = 0; i < data.length; i += 4) {
+                        data[i] = data[i] ^ 255; // Invert Red
+                        data[i + 1] = data[i + 1] ^ 255; // Invert Green
+                        data[i + 2] = data[i + 2] ^ 255; // Invert Blue
                     }
-                });
+                    ctx.putImageData(imageData, 0, 0);
+                    ctx.font = 'bolder 12px "helvetica", sans-serif';
+                    ctx.fillStyle = '#ffffff';
+                    ctx.fillText(unreadCount, 4, 12);
+                }
+                links.forEach(link => {link.href = canvas.toDataURL('image/png');})
             };
-            setTitles();
-            setInterval(setTitles, 60000);
+            img.src = origLink;
+        }, 1000);
+    }
 
+    initialize();
 
-            setInterval(function () {
-                const unreadElements = document.querySelectorAll('#sidebarChannelContainer .unread-title');
-                createDirectUnreadLinks(sideBarEl, unreadElements);
-
-                var unreadCount = unreadElements.length
-                    + document.querySelectorAll('.team-btn .badge').length
-                    + document.querySelectorAll('.team-container.unread').length;
-                canvas.height = canvas.width = 16; // set the size
-                ctx = canvas.getContext('2d');
-                img.onload = function () { // once the image has loaded
-                    ctx.drawImage(this, 0, 0);
-                    sideBarEl.style.backgroundColor = sideBarColors.old;
-                    if (unreadCount > 0) {
-                        sideBarEl.style.backgroundColor = sideBarColors.new;
-                        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                        var data = imageData.data;
-                        for (var i = 0; i < data.length; i += 4) {
-                            data[i] = data[i] ^ 255; // Invert Red
-                            data[i + 1] = data[i + 1] ^ 255; // Invert Green
-                            data[i + 2] = data[i + 2] ^ 255; // Invert Blue
-                        }
-                        ctx.putImageData(imageData, 0, 0);
-                        ctx.font = 'bolder 12px "helvetica", sans-serif';
-                        ctx.fillStyle = '#ffffff';
-                        ctx.fillText(unreadCount, 4, 12);
-                    }
-                    links.forEach(link => {link.href = canvas.toDataURL('image/png');})
-                };
-                img.src = origLink;
-            }, 1000);
-        }, 10000);
-
-
-    }, false);
+    //    }, false);
 })(document);
